36614e11761b8a2af139f5351d3f4c38693e8a52
Joao Felipe <joao.felipe.alves.m@gmail.com>
2024-05-30 02:56:08 -0300

    Created a custom MUI theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { ThemeProvider } from '@mui/material';
+import Light from './shared/themes/Light.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <ThemeProvider theme={Light}>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
+);
---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,10 @@
+import { createTheme } from '@mui/material/styles';
+import { ptBR } from '@mui/material/locale';
+
+const theme = createTheme({
+    typography: {
+        fontFamily: 'Arial, Roboto, sans-serif',
+    },
+}, ptBR);
+
+export default theme;
